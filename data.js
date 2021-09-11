595d8683c4d09c2c9bea65758c81cb2bcf4e7250
wellmoredrone <85592388+wellmoredrone@users.noreply.github.com>
2021-09-11 15:05:57 -0400

    init

---
+++ data.js
@@ -0,0 +1,99 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-area-1",
+      "name": "Area 1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        }
+      ],
+      "faceSize": 4096,
+      "initialViewParameters": {
+        "yaw": 0.7857495849198166,
+        "pitch": 0.722199981636555,
+        "fov": 1.4247463008564816
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.019976295077288597,
+          "pitch": 0.44691849403421635,
+          "rotation": 0,
+          "target": "1-area-2"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-area-2",
+      "name": "Area 2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        },
+        {
+          "tileSize": 512,
+          "size": 4096
+        },
+        {
+          "tileSize": 512,
+          "size": 8192
+        }
+      ],
+      "faceSize": 5750,
+      "initialViewParameters": {
+        "yaw": 0.5290020464574106,
+        "pitch": 0.7664010089197681,
+        "fov": 1.0032067764661057
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.22789575445590415,
+          "pitch": -0.02001128304328681,
+          "rotation": 0,
+          "target": "0-area-1"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "8-30-21 Flood Damage as of 9-9-21",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
